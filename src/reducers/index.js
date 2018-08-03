4ba8acdea7395a1dd5d2c47246d9eab681f8e653
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-08-03 11:02:49 +0000

    automatically backed up by learn

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,11 @@
 export default function ingredientReducer() {
+  switch(action.type) {
+   case 'ADD_INGREDIENT':
+     return [...state, action.payload];
+   case 'REMOVE_INGREDIENT':
+     return state.filter((item) => item !== action.payload);
+   default:
+     return state;
+ }
 
 }
